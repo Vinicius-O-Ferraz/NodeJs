cd7f56e7672bf6ba24647977465a360c28757902
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2025-01-01 20:19:16 -0300

    final: routing

--- 3_Core_Modules/8_routing/index.js
+++ 3_Core_Modules/8_routing/index.js
@@ -10,14 +10,17 @@ const server = http.createServer((req,res)=>{
 
     if(filename.includes('html')){
         if(fs.existsSync(filename)) {
-            console.log(filename)
             fs.readFile(filename, function (err, data) {
                 res.writeHead(200, { 'Content-Type': 'text/html' });
                 res.write(data);
                 return res.end();
             });
         }else{
-            console.log("Deve ter errado o nome do artigo")
+            fs.readFile('404.html', function (err, data) {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.write(data);
+                return res.end();
+            });
         }
     }
 })
