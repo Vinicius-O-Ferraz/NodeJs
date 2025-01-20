85e21df49caf2a30fececafe05c6048665af9815
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2025-01-20 13:02:39 -0300

    fix: tarefa

---
+++ 6_Express/Tarefa/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+const port = 5000
+
+const path = require("path")
+const basePath = path.join(__dirname,  'templates')
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(basePath, 'lista_de_compras.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Servidor excutando na porta 5000`)
+});
--- 6_Express/Tarefa/src/server.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.listen(5000, () => {
-    console.log(`Servidor excutando na porta 5000`)
-});
